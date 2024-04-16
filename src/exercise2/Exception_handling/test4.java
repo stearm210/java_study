1f357e1ba1dfb083db232b6a3a63eea654636c87
stearm210 <pyzxwoo@163.com>
2024-04-16 19:57:04 +0800

    更改

---
+++ src/exercise2/Exception_handling/test4.java
@@ -0,0 +1,28 @@
+package exercise2.Exception_handling;
+
+import java.util.Scanner;
+
+public class test4 {
+    public static void main(String[] args) {
+        //使用ctrl+alt+t 实现循环、try-catch快捷键选择
+        while (true) {
+            try {
+                System.out.println(getMoney());
+            } catch (Exception e) {
+                System.out.println("请输入合法的数字");
+            }
+        }
+    }
+    public static double getMoney(){
+        Scanner sc=new Scanner(System.in);
+        while (true){
+            System.out.println("输入合适的价格");
+            double money=sc.nextDouble();
+            if(money>=0){
+                return money;
+            }else {
+                System.out.println("输入的不合理");
+            }
+        }
+    }
+}
