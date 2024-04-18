52cd3ab60dc769b7e5c60869fc7df18c70183b57
stearm210 <pyzxwoo@163.com>
2024-04-18 14:21:52 +0800

    更改

---
+++ src/exercise2/ArryList_LinkedList/ListTest1.java
@@ -0,0 +1,24 @@
+package exercise2.ArryList_LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest1 {
+    public static void main(String[] args) {
+        //创建一个ArrayList集合对象
+        //下面是一个经典代码
+        List<String> list=new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("ddd");
+        System.out.println(list);
+        //在某个索引位置插入元素
+
+        //根据索引删除元素，返回删除的元素
+
+        //返回集合中指定位置的元素、
+
+        //修改索引处的元素，修改成功的会返回对应的数据
+    }
+}
