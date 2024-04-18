549ff36a2f860da9f5565bf5cec5ac58224a0242
stearm210 <pyzxwoo@163.com>
2024-04-18 14:24:38 +0800

    更改

--- src/exercise2/ArryList_LinkedList/ListTest1.java
+++ src/exercise2/ArryList_LinkedList/ListTest1.java
@@ -13,12 +13,20 @@ public class ListTest1 {
         list.add("ccc");
         list.add("ddd");
         System.out.println(list);
+
         //在某个索引位置插入元素
+        list.add(2,"kkk");
+        System.out.println(list);
 
         //根据索引删除元素，返回删除的元素
+        System.out.println(list.remove(2));
+        System.out.println(list);
 
         //返回集合中指定位置的元素、
+        System.out.println(list.get(3));
 
         //修改索引处的元素，修改成功的会返回对应的数据
+        System.out.println(list.set(3, "www"));
+        System.out.println(list);
     }
 }
