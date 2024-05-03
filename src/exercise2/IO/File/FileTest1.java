afda038dfcbde4334c297ed1cf51ddd02266f67a
stearm210 <pyzxwoo@163.com>
2024-05-03 15:02:58 +0800

    更改

--- src/exercise2/IO/File/FileTest1.java
+++ src/exercise2/IO/File/FileTest1.java
@@ -5,6 +5,13 @@ import java.io.File;
 public class FileTest1 {
     public static void main(String[] args) {
         //创建对象，指代具体的文件
+        //下面是文件路径的不同写法
+        //File f1 = new File("D:/java study/data/ab.txt");
+        //File f1 = new File("D:"+File.separator+"resource"+File.separator+"ab.txt");
+
         File f1 = new File("D:\\java study\\data\\ab.txt");
+        System.out.println(f1.length());//文件大小
+
+
     }
 }
