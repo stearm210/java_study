71416d7f0aa15f772e9b6a218bf4b5a0fa94688b
stearm210 <pyzxwoo@163.com>
2024-05-03 20:17:34 +0800

    更改

---
+++ src/exercise2/IO/File/FileTest7.java
@@ -0,0 +1,23 @@
+package exercise2.IO.File;
+
+import java.io.File;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise2.IO.File
+ * @Project：study
+ * @name：FileTest7
+ * @Date：3/5/2024 20:15
+ * @Filename：FileTest7
+ */
+
+//文件的遍历删除操作
+public class FileTest7 {
+    public static void main(String[] args) {
+        //目标是删除非空文件夹
+
+    }
+    public static void deleteDir(File dir){
+
+    }
+}
