e5a67c4f553eb5051555d85b4a0b58b70497a81a
stearm210 <pyzxwoo@163.com>
2024-05-03 14:57:28 +0800

    更改

--- src/exercise2/IO/File/FileTest1.java
+++ src/exercise2/IO/File/FileTest1.java
@@ -1,4 +1,10 @@
 package exercise2.IO.File;
 
+import java.io.File;
+
 public class FileTest1 {
+    public static void main(String[] args) {
+        //创建对象，指代具体的文件
+        File f1 = new File("D:\\java study\\data\\ab.txt");
+    }
 }
