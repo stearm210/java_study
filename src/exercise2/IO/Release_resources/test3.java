09e02e1d64f2426bdc71b8dc8caf8e69fbb6dd32
stearm210 <pyzxwoo@163.com>
2024-05-05 15:39:36 +0800

    更改

--- src/exercise2/IO/Release_resources/test3.java
+++ src/exercise2/IO/Release_resources/test3.java
@@ -15,19 +15,22 @@ import java.io.*;
 public class test3 {
     public static void main(String[] args) {
         try (//1.创建一个字符的输入流
-             InputStream is=new FileInputStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\IO_steam\\itheima04.txt");
-
+             InputStream is = new FileInputStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\IO_steam" +
+                     "\\itheima04.txt");
              //2.创建一个字符的输出流
-             OutputStream os=new FileOutputStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\IO_steam\\itheima04.txt");){
+             OutputStream os = new FileOutputStream("D:\\java study\\IDEApro " +
+                     "code\\study\\src\\exercise2\\IO\\IO_steam\\itheima04.txt");) {
+
             //3.创建一个字节数组
-            byte[] buffer=new byte[1024];
+            byte[] buffer = new byte[1024];
             //4.从字节输入流中读取字节，写到字节输出流中，读多少写多少
             int len;//记录每次读取了多少字节
-            while ((len=is.read(buffer)) != -1){
-                os.write(buffer,0,len);
+            while ((len = is.read(buffer)) != -1) {
+                os.write(buffer, 0, len);
             }
             System.out.println("复制完成");
-        }catch (IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
