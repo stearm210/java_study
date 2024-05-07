263a65380081782a6aefadc53508f8d03e492e5a
stearm210 <pyzxwoo@163.com>
2024-05-07 20:25:45 +0800

    更改

---
+++ src/exercise2/IO/Conversion_Flow/ConversionFlowTest3.java
@@ -0,0 +1,17 @@
+package exercise2.IO.Conversion_Flow;
+
+//获取字节输出流，再按照指定的字符集编码将其转换成字符输出流，以后写出去的字符就会用该字符集编码了
+
+/**
+ * @Author：stearm210
+ * @Package：exercise2.IO.Conversion_Flow
+ * @Project：study
+ * @name：ConversionFlowTest3
+ * @Date：7/5/2024 20:24
+ * @Filename：ConversionFlowTest3
+ */
+public class ConversionFlowTest3 {
+    public static void main(String[] args) {
+        //1.创建一个文件字符输出流
+    }
+}
