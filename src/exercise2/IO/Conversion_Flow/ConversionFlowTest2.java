7df2b80ddabd6320116a9ada82b2018ddd477e40
stearm210 <pyzxwoo@163.com>
2024-05-07 19:55:06 +0800

    更改

---
+++ src/exercise2/IO/Conversion_Flow/ConversionFlowTest2.java
@@ -0,0 +1,32 @@
+package exercise2.IO.Conversion_Flow;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise2.IO.Conversion_Flow
+ * @Project：study
+ * @name：ConversionFlowTest2
+ * @Date：7/5/2024 19:54
+ * @Filename：ConversionFlowTest2
+ */
+public class ConversionFlowTest2 {
+    public static void main(String[] args) {
+        //1.创建一个文件字符输入流与源文件接通
+        //代码编码为UTF-8    文件的编码为UTF-8
+        try (
+                Reader fr=new FileReader("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\IO_steam\\itheima03.txt");
+                //2.这里将文件字符输入流包装成缓冲字符输入流,提高性能
+                BufferedReader br=new BufferedReader(fr);
+        ){
+            String line;//记录读取的行数
+            while ((line=br.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
