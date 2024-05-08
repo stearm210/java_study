b8cbd25b06cc191fa9b8a1200b73574c3c9e70df
stearm210 <pyzxwoo@163.com>
2024-05-08 20:06:31 +0800

    更改

---
+++ src/exercise2/IO/Print_Stream/PrintStreamTest2.java
@@ -0,0 +1,18 @@
+package exercise2.IO.Print_Stream;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise2.IO.Print_Stream
+ * @Project：study
+ * @name：PrintStreamTest2
+ * @Date：8/5/2024 20:05
+ * @Filename：PrintStreamTest2
+ */
+public class PrintStreamTest2 {
+    public static void main(String[] args) {
+        System.out.println("老骥伏枥");
+        System.out.println("志在千里");
+        System.out.println("烈士暮年");
+        System.out.println("壮心不已");
+    }
+}
