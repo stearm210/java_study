399558fc91ca0deaf672f90c955ef1210a020b27
stearm210 <pyzxwoo@163.com>
2024-05-08 20:14:15 +0800

    更改

--- src/exercise2/IO/Print_Stream/PrintStreamTest2.java
+++ src/exercise2/IO/Print_Stream/PrintStreamTest2.java
@@ -1,5 +1,8 @@
 package exercise2.IO.Print_Stream;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  * @Author：stearm210
  * @Package：exercise2.IO.Print_Stream
@@ -12,7 +15,17 @@ public class PrintStreamTest2 {
     public static void main(String[] args) {
         System.out.println("老骥伏枥");
         System.out.println("志在千里");
-        System.out.println("烈士暮年");
-        System.out.println("壮心不已");
+
+        //创建一个打印流，将上面的东西打印进一个文件中去
+        try (
+                PrintStream ps = new PrintStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\Print_Stream\\itheima02.txt");
+                ){
+            //将系统默认的打印流改成自己设置的
+            System.setOut(ps);
+            System.out.println("烈士暮年");
+            System.out.println("壮心不已");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
