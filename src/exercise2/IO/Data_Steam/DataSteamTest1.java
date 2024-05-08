e7e3d2ac4fa260c06eca055cac84f835a8d8f38d
stearm210 <pyzxwoo@163.com>
2024-05-08 20:23:14 +0800

    更改

---
+++ src/exercise2/IO/Data_Steam/DataSteamTest1.java
@@ -0,0 +1,15 @@
+package exercise2.IO.Data_Steam;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise2.IO.Data_Steam
+ * @Project：study
+ * @name：DataSteamTest1
+ * @Date：8/5/2024 20:22
+ * @Filename：DataSteamTest1
+ */
+public class DataSteamTest1 {
+    public static void main(String[] args) {
+
+    }
+}
