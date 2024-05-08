75fd572da267b06531346f965b6dbf1d6f9a0be0
stearm210 <pyzxwoo@163.com>
2024-05-08 20:28:33 +0800

    更改

--- src/exercise2/IO/Data_Steam/DataSteamTest1.java
+++ src/exercise2/IO/Data_Steam/DataSteamTest1.java
@@ -1,5 +1,9 @@
 package exercise2.IO.Data_Steam;
 
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
  * @Author：stearm210
  * @Package：exercise2.IO.Data_Steam
@@ -10,6 +14,17 @@ package exercise2.IO.Data_Steam;
  */
 public class DataSteamTest1 {
     public static void main(String[] args) {
+        try (
+                //1.包装一个低级的数据输出流
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\Data_Steam\\itheima01.txt"));
+                ){
+            dos.write(97);
+            dos.writeDouble(99.5);
+            dos.writeBoolean(true);
+            dos.writeUTF("哈哈哈哈哈9999！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
