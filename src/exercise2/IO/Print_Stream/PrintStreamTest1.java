99db8791370087ae3fb2ce795ab00f48d5771625
stearm210 <pyzxwoo@163.com>
2024-05-08 19:59:18 +0800

    更改

---
+++ src/exercise2/IO/Print_Stream/PrintStreamTest1.java
@@ -0,0 +1,23 @@
+package exercise2.IO.Print_Stream;
+
+
+//执行打印流的方法
+
+import java.io.PrintStream;
+public class PrintStreamTest1 {
+    public static void main(String[] args) {
+
+        try (
+                //1.创建一个打印流对象
+                PrintStream ps = new PrintStream("D:\\java study\\IDEApro code\\study\\src\\exercise2\\IO\\Print_Stream\\itheima01.txt");
+                ){
+            ps.println(97);
+            ps.println('a');
+            ps.println("对对对");
+            ps.println(true);
+            ps.println(99.5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
