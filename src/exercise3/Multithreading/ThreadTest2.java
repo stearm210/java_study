d451aaf7e10b258ca4773c1f6a53defcd8db750c
stearm210 <pyzxwoo@163.com>
2024-05-21 15:08:28 +0800

    更改

---
+++ src/exercise3/Multithreading/ThreadTest2.java
@@ -0,0 +1,23 @@
+package exercise3.Multithreading;
+
+
+/*
+* 1.创建多线程的第二种方式,实现Runable接口
+*
+*
+* */
+
+
+/**
+ * @Author：stearm210
+ * @Package：exercise3.Multithreading
+ * @Project：study
+ * @name：ThreadTest2
+ * @Date：21/5/2024 15:07
+ * @Filename：ThreadTest2
+ */
+public class ThreadTest2 {
+    public static void main(String[] args) {
+
+    }
+}
