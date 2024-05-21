18395907ae125ff739889063d59728de57243c83
stearm210 <pyzxwoo@163.com>
2024-05-21 16:24:15 +0800

    更改

--- src/exercise3/Multithreading/MyCallable.java
+++ src/exercise3/Multithreading/MyCallable.java
@@ -12,6 +12,8 @@ package exercise3.Multithreading;
 */
 
 
+import java.util.concurrent.Callable;
+
 /**
  * @Author：stearm210
  * @Package：exercise3.Multithreading
@@ -23,5 +25,23 @@ package exercise3.Multithreading;
 
 /*
 * 1.实现callable接口*/
-public class MyCallable {
+public class MyCallable implements Callable<String> {
+    private int n;
+
+    public MyCallable(int n) {
+        this.n = n;
+    }
+
+    //线程具体的任务、操作都会通过这个call方法进行体现
+    //2.重写call方法
+    @Override
+    public String call() throws Exception {
+        //描述线程的任务，返回线程执行返回后的结果
+        //需求，求1-n的和并返回
+        int sum=0;
+        for (int i = 0; i <=n; i++) {
+            sum+=i;
+        }
+        return "线程求出的和为:"+sum;
+    }
 }
--- src/exercise3/Multithreading/ThreadTest3.java
+++ src/exercise3/Multithreading/ThreadTest3.java
@@ -1,5 +1,10 @@
 package exercise3.Multithreading;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
 /**
  * @Author：stearm210
  * @Package：exercise3.Multithreading
@@ -9,7 +14,20 @@ package exercise3.Multithreading;
  * @Filename：ThreadTest3
  */
 public class ThreadTest3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //3.创建一个Callable对象
+        Callable<String> call=new MyCallable(100);
+        //4.将Callable的对象封装成一个FutureTask对象（任务对象）
+        //(1)是一个任务对象，实现了Runnable对象
+        //(2)可以在线程执行完毕以后，用未来任务对象调用get方法获取线程执行完毕之后的结果
+        FutureTask<String> fl = new FutureTask<>(call);
 
+        //5.将任务对象交给一个线程Thread对象
+        new Thread(fl).start();
+
+
+        //6.获取线程执行完毕之后的返回结果
+        String rs=fl.get();
+        System.out.println(rs);
     }
 }
