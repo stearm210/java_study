dfa2f76769e31c624edf8795cff05353177cf645
stearm210 <pyzxwoo@163.com>
2024-05-22 14:54:19 +0800

    更改

---
+++ src/exercise3/Multithreading/Thread_Safety/Account.java
@@ -0,0 +1,43 @@
+package exercise3.Multithreading.Thread_Safety;
+
+/*
+* 账户中有10万元，模拟当有两个人同时取钱时的账户安全问题
+* */
+
+
+/**
+ * @Author：stearm210
+ * @Package：exercise3.Multithreading.Thread_Safety
+ * @Project：study
+ * @name：Account
+ * @Date：22/5/2024 14:52
+ * @Filename：Account
+ */
+public class Account {
+    private double money;//余额
+    private String cardId;//卡号
+
+    public Account() {
+    }
+
+    public Account(double money, String cardId) {
+        this.money = money;
+        this.cardId = cardId;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+}
---
+++ src/exercise3/Multithreading/Thread_Safety/ThreadTest.java
@@ -0,0 +1,15 @@
+package exercise3.Multithreading.Thread_Safety;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise3.Multithreading.Thread_Safety
+ * @Project：study
+ * @name：ThreadTest
+ * @Date：22/5/2024 14:51
+ * @Filename：ThreadTest
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+
+    }
+}
