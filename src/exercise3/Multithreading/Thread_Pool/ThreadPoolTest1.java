74429b9d575614c500098f176ae05e1cd6d11dcb
stearm210 <pyzxwoo@163.com>
2024-05-23 16:48:30 +0800

    更改

---
+++ src/exercise3/Multithreading/Thread_Pool/ThreadPoolTest1.java
@@ -0,0 +1,18 @@
+package exercise3.Multithreading.Thread_Pool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @Author：stearm210
+ * @Package：exercise3.Multithreading.Thread_Pool
+ * @Project：study
+ * @name：ThreadPoolTest1
+ * @Date：23/5/2024 16:46
+ * @Filename：ThreadPoolTest1
+ */
+public class ThreadPoolTest1 {
+    public static void main(String[] args) {
+        //1.使用构造器创建一个线程池对象
+        new ThreadPoolExecutor();
+    }
+}
