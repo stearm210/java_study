73f76a0dff54a1324650295664889666abb8c413
stearm210 <pyzxwoo@163.com>
2024-05-24 18:47:24 +0800

    更改

--- src/exercise3/Network_Programming/UDP/Client.java
+++ src/exercise3/Network_Programming/UDP/Client.java
@@ -5,6 +5,7 @@ package exercise3.Network_Programming.UDP;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * @Author：stearm210
@@ -23,8 +24,15 @@ public class Client {
         //这是一个有参构造，传入的参数有
         //参数一：封装要发出去的数据
         //参数二：发出去的数据大小(字节个数)
+        //参数三：服务端的IP地址
+        //参数四：服务端程序的端口
         byte[] bytes="这是一个封装之后需要发出去的信息".getBytes();
-        DatagramPacket packet=new DatagramPacket();
+        DatagramPacket packet=new DatagramPacket(bytes,bytes.length, InetAddress.getLocalHost(),6666);
 
+        //3.开始正式发送信息
+        socket.send(packet);
+        System.out.println("客户端数据发送完毕");
+        //释放系统资源
+        socket.close();
     }
 }
