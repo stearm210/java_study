d89cea697430b7e99d005474393ec338ab6d9a24
stearm210 <pyzxwoo@163.com>
2024-05-24 19:04:34 +0800

    更改

--- src/exercise3/Network_Programming/UDP/Server.java
+++ src/exercise3/Network_Programming/UDP/Server.java
@@ -1,5 +1,14 @@
 package exercise3.Network_Programming.UDP;
 
+
+//创建一个服务端进行数据的接收服务等
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Date;
+
 /**
  * @Author：stearm210
  * @Package：exercise3.Network_Programming.UDP
@@ -9,4 +18,21 @@ package exercise3.Network_Programming.UDP;
  * @Filename：Server
  */
 public class Server {
+    public static void main(String[] args) throws IOException {
+        //1.创建一个服务端对象(创建一个接数据的人)。注册一个端口
+        DatagramSocket socket=new DatagramSocket(6666);
+
+        //2.创建一个数据包对象，主要用于接收数据
+        byte[] buffer=new byte[1024*64];
+        DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
+
+        //3.使用数据包对数据进行接收
+        socket.receive(packet);
+
+        //4.从字节数组中将接收的数据直接打印出来
+        //实现获取多少就输出多少
+        int len=packet.getLength();
+        String rs = new String(buffer,0,len);
+        System.out.println(rs);
+    }
 }
